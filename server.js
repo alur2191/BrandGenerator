72bf705cfd849080f6ae84e5dfe0278f94b7a55e
dandavisjs <dandavisjs@protonmail.com>
2022-03-07 20:11:17 -0800

    user registration and JWT configured

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('API Running'))
+// Connect Database
+connectDB();
 
-const PORT = process.env.PORT || 5000
+// Init Middleware
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+// app.use('/api/brand', require('./routes/api/brand'))
+// app.use('/api/personas', require('./routes/api/personas'))
+// app.use('/api/competitors', require('./routes/api/competitors'))
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
